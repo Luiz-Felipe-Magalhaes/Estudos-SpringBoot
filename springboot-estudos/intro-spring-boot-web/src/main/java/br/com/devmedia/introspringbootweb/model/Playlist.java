5e263c1eded81df7f89d41e7c6d6add792a82d80
Luiz Felipe Magalhães <luizfelipe.magalhaes@outlook.com>
2023-10-06 22:54:40 -0300

    Criação de projetos Rest basicos em spring

---
+++ springboot-estudos/intro-spring-boot-web/src/main/java/br/com/devmedia/introspringbootweb/model/Playlist.java
@@ -0,0 +1,73 @@
+package br.com.devmedia.introspringbootweb.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.BatchSize;
+import org.hibernate.annotations.Columns;
+
+import com.sun.istack.NotNull;
+
+@Entity
+@Table(name = "playlists")
+public class Playlist {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@NotBlank
+	@Size(min = 2, max = 60)
+	@Column(nullable = false, length = 60)
+	private String nome;
+
+	@NotBlank
+	@Column(nullable = false)
+	private String descricao;
+
+	@OneToMany(mappedBy = "playlist", cascade = CascadeType.ALL)
+	private List<Musica> musicas;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public List<Musica> getMusicas() {
+		return musicas;
+	}
+
+	public void setMusicas(List<Musica> musicas) {
+		this.musicas = musicas;
+	}
+
+}
